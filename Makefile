abed0ced35b26a2bb2fbea495c0c16448e72853a
email <krista5055@yahoo.com>
2021-11-29 19:11:46 -0800

    steps 1-16

---
+++ Makefile
@@ -0,0 +1,2 @@
+kilo: kilo.c
+	$(CC) kilo.c -o kilo -Wall _Wextra -pedantic -std=c99
---
+++ kilo.c
@@ -0,0 +1,42 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <ubistd.h>
+
+struct termios orig_termios;
+
+void disablerawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP| IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= `(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(){
+    enableRawMode();
+
+    while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+        if (iscntrl9c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
+    return 0;
+}
