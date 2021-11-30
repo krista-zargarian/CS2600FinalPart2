ca6c3b3d5ed03426b1007e6268f570a346771951
email <krista5055@yahoo.com>
2021-11-30 13:17:39 -0800

    steps 35-37

--- kilo.c
+++ kilo.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys.ioctl.h>
 #include <termios.h>
 #include <ubistd.h>
@@ -86,11 +87,35 @@ int getWindowSize(int *rows, int *cols){
         return 0;
     }
 }
+//***append buffer***//
+struct abuf{
+    char *b;
+    int len;
+};
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    memcpy(&new[ab->len}, s, len);
+    ab->b = new;
+    ab-> += len;)
+}
+
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
+
 
 void editorDrawRows(){
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenrows -1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 //***output***//
