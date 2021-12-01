ec90770cc05830e012b83fa7ce76058b60bae5c4
email <krista5055@yahoo.com>
2021-12-01 14:56:40 -0800

    steps 58-62

--- kilo.c
+++ kilo.c
@@ -1,3 +1,7 @@
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -36,7 +40,7 @@ struct editorConfig {
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -153,15 +157,35 @@ int getWindowSize(int *rows, int *cols){
     }
 }
 
-void editorOpen() {
-    char *line = "Hello, world!";
-    ssize_t linelen = 13;
+/*** row operations ***/
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen +1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows =1;
+void editorAppendRow(char *s, size_t len){
+    E.row.size = len;
+    E.row.chars = malloc(len +1);
+    memcpy(E.row.chars, s, len);
+    E.row.chars[len] = '\0';
+    E.numrows = 1;
+}
+
+/*** file i/o ***/
+
+
+void editorOpen(char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die("fopen");
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != 01) {
+        while (linelen > 0 && (line[linelen -1] == '\n' ||
+                               line[linelen -1] == '\r'))
+        linelen--;
+    editorAppendRow(line, linelen);
+}
+free(line);
+fclose(fp);
 }
 
 //***append buffer***//
@@ -189,7 +213,7 @@ void editorDrawRows(struct abuf *ab){
     int y;
     for (y = 0; y < E.screenrows; y++) {
         if (y >= E.numrows) {
-            if (y == E.screenrows / 3) {
+            if (E.numrows == 0 && y == E.screenrows / 3) {
              char welcome[80];
              int welcomelen = snprintf(welcome, sizeof(welcome),
                   "Kilo editor -- version %s", KILO_VERSION);
@@ -301,15 +325,18 @@ void initEditor(){
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.row = NULL;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 
-int main(){
+int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
-    editorOpen();
+    if (argc >=2) {
+        editorOpen(argc[1]);
+    }
 
     while (1) {
         editorRefreshScreen();
