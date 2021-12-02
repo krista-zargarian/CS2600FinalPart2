a275aa4cd02acaf9d7724df041d1c75e9797443a
email <krista5055@yahoo.com>
2021-12-01 18:03:09 -0800

    steps 71-73

--- kilo.c
+++ kilo.c
@@ -38,6 +38,7 @@ typedef struct erow {
 struct editorConfig {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -218,6 +219,8 @@ void editorScroll() {
     if (E.cy >= E.rowoff + E.screenrows) {
         E.rowoff = E.cy - E.screenrows + 1;
     }
+    if (E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
 }
 
 
@@ -243,9 +246,10 @@ void editorDrawRows(struct abuf *ab){
                   abAppend(ab, "~", 1);
              }
          } else {
-             int len = E.row[filerow].size;
+             int len = E.row[filerow].size - E.coloff;
+             if (len < 0) len = 0;
              if (len > E.screencols) len = E.screencols;
-             abAppend(ab, E.row[filerow].chars, len);
+             abAppend(ab, &E.row[filerow].chars[E.coloff], len);
          }
 
         abAppend(ab, "\x1b[K", 3);
@@ -341,6 +345,7 @@ void initEditor(){
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
