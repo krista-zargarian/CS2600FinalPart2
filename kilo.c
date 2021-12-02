ab2a3f5370a08f3815af0a2da36d2072aef028ec
email <krista5055@yahoo.com>
2021-12-02 08:22:29 -0800

    steps 78-82

--- kilo.c
+++ kilo.c
@@ -32,7 +32,9 @@ struct termios orig_termios;
 
 typedef struct erow {
     int size;
+    int rsize;
     char *chars;
+    char *render;
 } erow;
 
 struct editorConfig {
@@ -161,6 +163,19 @@ int getWindowSize(int *rows, int *cols){
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row){
+    free(row->render);
+    row->render = malloc(row->size + 1);
+
+    int j;
+    int idx = 0;
+    for (j = 0; j < row->size; j++) {
+        row->render[idx++] = row->chars[j];
+    }
+    row->render[idx] = '\0';
+    row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
     E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -169,6 +184,11 @@ void editorAppendRow(char *s, size_t len) {
     E.row[at].chars = malloc(len + 1);
     memcpy(E.row[at].chars, s, len);
     E.row[at].chars[len] = '\0';
+
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
+
     E.numrows++;
 }
 
@@ -246,10 +266,10 @@ void editorDrawRows(struct abuf *ab){
                   abAppend(ab, "~", 1);
              }
          } else {
-             int len = E.row[filerow].size - E.coloff;
+             int len = E.row[filerow].rsize - E.coloff;
              if (len < 0) len = 0;
              if (len > E.screencols) len = E.screencols;
-             abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+             abAppend(ab, &E.row[filerow].render[E.coloff], len);
          }
 
         abAppend(ab, "\x1b[K", 3);
@@ -285,11 +305,17 @@ void editorMoveCursor(int key) {
         case ARROW_LEFT:
             if (E.cx != 0) {
                 E.cx--;
+            } else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
             }
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size) {
                 E.cx++;
+            } else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
             }
             break;
         case ARROW_UP:
