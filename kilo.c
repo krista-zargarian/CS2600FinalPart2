01544f1f31609fbeb2f1215e626e9364be59a077
email <krista5055@yahoo.com>
2021-12-01 18:31:15 -0800

    fixed errors 5

--- kilo.c
+++ kilo.c
@@ -357,7 +357,7 @@ int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
     if (argc >=2) {
-        editorOpen(argc[1]);
+        editorOpen(argv[1]);
     }
 
     while (1) {
