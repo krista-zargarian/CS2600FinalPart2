a6aaf3720342c45259662c61196422d11496d1c2
email <krista5055@yahoo.com>
2021-12-04 12:51:31 -0800

    steps 152-155

--- kilo.c
+++ kilo.c
@@ -186,15 +186,30 @@ int getWindowSize(int *rows, int *cols){
 
 /*** syntax highlighting ***/
 
+int is_sepatator(int c) {
+    return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;  
+}
+
 void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->size);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    int i;
-    for (i = 0; i < row->rsize; i++) {
-        if (isdigit(row->render[i])) {
+    int prev_step = 1;
+
+    int i = 0;
+    while (i < row->rsize) {
+        char c = row->render[i];
+        unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if (isdigit(c) && (prev_step || prev_hl == HL_NUMBER)) {
             row->hl[i] = HL_NUMBER;
+            i++;
+            prev_step = 0;
+            continue;
         }
+
+        prev_step = is_separator(c);
+        i++;
     }
 }
 
@@ -435,6 +450,15 @@ void editorFindCallback(char *query, int key) {
     static int last_match = -1;
     static int direction = 1;
 
+    static int saved_hl_line;
+    static char *saved_hl = NULL;
+
+    if (saved_hl) {
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
     if (key == '\r' || key == '\x1b') {
         last_match = -1;
         direction = 1;
@@ -465,6 +489,9 @@ void editorFindCallback(char *query, int key) {
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
 
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
             memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
