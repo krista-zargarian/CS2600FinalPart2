c4d4129f8b7f841bcbc30bd15f4097d761a182d4
email <krista5055@yahoo.com>
2021-12-05 15:49:56 -0800

    corrections again

--- kilo.c
+++ kilo.c
@@ -93,8 +93,9 @@ struct editorConfig E;
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
 
 char *C_HL_keywords[] = {
-  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "switch", "if", "while|", "for", "break", "continue", "return", "else",
   "struct", "union", "typedef", "static", "enum", "class", "case",
+
   "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
   "void|", NULL
 };
