73c6a7301dccba3e14180eaf70fb80494bef8659
email <krista5055@yahoo.com>
2021-12-04 17:22:25 -0800

    fixed colors 2

--- kilo.c
+++ kilo.c
@@ -93,11 +93,10 @@ struct editorConfig E;
 
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
 char *C_HL_keywords[] = {
-    "switch", "if", "while", "for", "break", "continue", "return", "else",
-    "struct", "union", "typedef", "static", "enum", "class", "case",
-
-    "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|", 
-    "void|", NULL
+  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "struct", "union", "typedef", "static", "enum", "class", "case",
+  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+  "void|", NULL
 };
 
 struct editorSyntax HLDB[] = {
